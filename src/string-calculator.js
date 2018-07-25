6cd2436a2792f05b9088de0bca1a0324b03ed439
br5g8ch <brandon_seich@gap.com>
2018-07-25 14:42:06 -0400

    adding two numbers

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,1 +1,3 @@
-export default () => 0
+export default (str) => str.split(',')
+    .map((val)=>parseInt((val || '0')))
+    .reduce((val, acc) => val + acc, 0)
--- tests/string-calculator-spec.js
+++ tests/string-calculator-spec.js
@@ -7,4 +7,11 @@ describe('String Calculator', () => {
         expect(calculate('')).to.be.equal(0)
     })
 
+    it('should return a number when provided a single number in the string', () => {
+        expect(calculate('74')).to.be.equal(74)
+    })
+
+    it('should add two numbers separated by delimeter', () => {
+        expect(calculate('4,5')).to.be.equal(9)
+    })
 })
