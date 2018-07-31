a647c53bd5069bdc3ca030d8e9c29825a7a5cbfb
John Morales <morales.201@osu.edu>
2018-07-31 00:22:05 -0400

    Refactoring...

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,35 +1,40 @@
-import { curry } from 'lodash';
+import { curry } from 'lodash'
 
 const calc = curry((delimeter, combiner, exp) => exp.split(delimeter)
     .map((val) => parseFloat((val || '0')) )
-    .reduce(combiner));
+    .reduce(combiner))
 
-const add = calc(/(?=\+|-)/, (acc, val) => val + acc);
-const multiply = calc('*', (acc, val) => acc * val);
-const divide = calc('/', (acc, val) => acc / val);
+const add = calc(/(?=\+|-)/, (acc, val) => val + acc)
+const multiply = calc('*', (acc, val) => acc * val)
+const divide = calc('/', (acc, val) => acc / val)
 
 const extractDivision = (exp) => exp.match(/[\d]+\/[\d]+/g)
 const extractMult = (exp) => exp.match(/\d+(\.\d+)?(\*\d+(\.\d+)?)+/g)
 
 const simplify = curry((calculation, exp, expressions) => {
     return expressions.reduce((simplifiedExp, curr) => {
-        return simplifiedExp.replace(curr, calculation(curr).toString());
+        return simplifiedExp.replace(curr, calculation(curr).toString())
     }, exp)
-});
+})
+
+const evaluate = (exp) => {
+    let expressions = extractDivision(exp)
+    if(Boolean(expressions)) {
+        exp = simplify(divide)(exp, expressions)
+    }
+
+    expressions = extractMult(exp)
+    if(Boolean(expressions)) {
+        exp = simplify(multiply)(exp, expressions)
+    }
+
+    return add(exp)
+}
+
+const replaceDivWithReciprocal = exp => exp.replace(/\//g, '*1/')
 
 export default (ogExp) => {
-    ogExp = ogExp.replace(/\//g, '*1/')
-    let expressions = extractDivision(ogExp);
-
-    if(Boolean(expressions)) {
-        ogExp = simplify(divide)(ogExp, expressions);
-    }
-
-    expressions = extractMult(ogExp);
-
-    if(Boolean(expressions)) {
-        ogExp = simplify(multiply)(ogExp, expressions);
-    }
-
-    return Math.round(add(ogExp));
+    return evaluate(replaceDivWithReciprocal(ogExp))
 }
+
+//Extract parantheses statement and simplify that first
